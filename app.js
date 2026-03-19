82d1e5bfdeb6f37597bd7bafd3ef5c6db8e1c552
BLA4CKK <lopesmachadopedrohenrique@gmail.com>
2026-03-19 16:01:36 -0300

    Refeita as rotas

--- app.js
+++ app.js
@@ -6,70 +6,92 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-app.get("/produtos", async (req, res) => {
-  const produtos = await sql`SELECT * FROM produtos`;
-  return res.status(200).json(produtos);
+//Area Usuario
+app.post("/cadastro", async (req, res) => {
+  const { senha, nome, email } = req.body;
+  console.log("cadastrado");
+  const cadastro =
+    await sql`INSERT INTO usuario(email, nome, senha) values(${email}, ${nome}, ${senha} )`;
+  return res.status(200).json(cadastro[0]);
 });
 
-
-app.get("/produtos/:id", async (req, res) => {
-  const { id } = req.params;
-  const produto = await sql`SELECT * FROM produtos WHERE id = ${id}`;
-  return res.status(200).json(produto[0]);
-});
-
-
-app.post("/Usuarios/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  const usuario = await sql`
-    SELECT * FROM usuario WHERE email = ${email} AND senha = ${password}
-  `;
-
-  if (usuario[0]) {
-   
-    const tipo = "user";
-    if (usuario[0].email === "admin@empresa.com") {
-      tipo = "admin";
+app.post("/login/", async (req, res) => {
+  const { email, senha } = req.body;
+  const entrar = await sql`select * from usuarios where email = ${email}`;
+  if (entrar[0]) {
+    const verificar =
+      await sql`select * from usuarios where email =${email} and senha= ${senha}`;
+    if (verificar[0]) {
+      return res.status(200).json(verificar[0]);
     }
-
-    return res.status(200).json({
-      email: usuario[0].email,
-      nome: usuario[0].nome,
-      tipo: tipo
-    });
-  }
-
-  return res.status(401).json({ mensagem: "Usuário ou senha incorretos" });
-});
-
-
-app.post("/agendamento", async (req, res) => {
-  const { nomep, numHospedes, valor } = req.body;
-
-  await sql`
-    INSERT INTO Agendamento (nome, numHospedes, valor)
-    VALUES (${nomep}, ${numHospedes}, ${valor})
-  `;
-  return res.status(201).json("Agendado com sucesso!");
-});
-
-
-app.post("/cadastrar", async (req, res) => {
-  const { nome, email, password } = req.body;
-  try {
-    await sql`
-      INSERT INTO usuario (nome, email, senha)
-      VALUES (${nome}, ${email}, ${password})
-    `;
-    return res.status(201).json({ mensagem: "Usuário cadastrado com sucesso!" });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ mensagem: "Erro ao cadastrar usuário" });
+    return res.status(401).json({ message: "Usuário ou senha incorreto" });
+  } else {
+    return res.status(404).json("Usuário não encontrado");
   }
 });
+
+app.get("/ListarUsers", async (req, res) => {
+  const listar = await sql`SELECT id_usuarios, nome FROM usuario;`
+  return res.status(200).json(listar)
+})
+
+//Area Reservas
+app.post("/Criar_Reserva", async (req, res) => {
+  const { } = req.body
+
+  const criar = await sql`INSERT INTO reservas(nome_completo, email, hotel, data_inicio, data_fim) VALUES(${urgencia}, ${funcao}, ${data}, ${localizacao}, ${id_usuario}, ${prazo}, ${responsavel})`;
+  return res.status(200).json(criar[0])
+})
+
+app.put("/Editar_Reserva/:id", async (req, res) => {
+  const { id } = req.params
+  const { } = req.body
+  const editar = await sql`UPDATE reservas
+	SET nivel_urgencia=${urgencia}, funcao=${funcao}, data_requisicao=${data}, localizacao=${localizacao}, prazo=${prazo}, destinatario_req=${responsavel}
+	WHERE id_requisicao = ${id}`
+  return res.status(200).json(editar)
+})
+
+app.delete("/Apagar_Reserva/:id", async (req, res) => {
+  const { id } = req.params
+  const apagar = await sql`DELETE FROM reservas
+WHERE id_requisicao = ${id};
+`
+  return res.status(200).json(apagar)
+})
+
+//Area Agendamentos
+app.post("/Criar_Agendamento", async (req, res) => {
+  const { } = req.body
+
+  const criar = await sql`INSERT INTO agendamentos(nome_completo, email, hotel, data_inicio, data_fim) VALUES(${urgencia}, ${funcao}, ${data}, ${localizacao}, ${id_usuario}, ${prazo}, ${responsavel})`;
+  return res.status(200).json(criar[0])
+})
+
+app.put("/Editar_Agendamento/:id", async (req, res) => {
+  const { id } = req.params
+  const { } = req.body
+  const editar = await sql`UPDATE agendamentos
+	SET nivel_urgencia=${urgencia}, funcao=${funcao}, data_requisicao=${data}, localizacao=${localizacao}, prazo=${prazo}, destinatario_req=${responsavel}
+	WHERE id_requisicao = ${id}`
+  return res.status(200).json(editar)
+})
+
+app.delete("/Apagar_Agendamento/:id", async (req, res) => {
+  const { id } = req.params
+  const apagar = await sql`DELETE FROM agendamentos
+WHERE id_requisicao = ${id};
+`
+  return res.status(200).json(apagar)
+})
 
 app.listen(3000, () => {
   console.log("Servidor rodando em http://localhost:3000");
 });
+
+// CREATE TABLE agendamentos (
+// id_agd SERIAL PRIMARY KEY,
+// nome_comp VARCHAR(255) NOT NULL,
+// email VARCHAR(255) UNIQUE NOT NULL
+// num_pessoas VARCHAR(30) NOT NULL,
+// forma_pag VARCHAR (50) NOT NULL 
